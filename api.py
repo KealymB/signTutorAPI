1d27f3c7aa4493a91ac03de69cf1d6c67e25d8c6
kealymmb <kealymmb@gmail.com>
2022-04-12 07:44:50 +0200

    Added image upload and game restart

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
+import io, base64
+from PIL import Image
+
 from glob import glob
 from operator import index
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -28,6 +31,32 @@ def get_letters():
 def clear_letters():
     global selectedIndecies
     selectedIndecies = [2,0,0,0,0]
+    return jsonify({
+        "selectedLetters": selectedLetters,
+        "selectedIndecies": selectedIndecies
+    })
+
+@app.route("/makeGuess", methods=["POST"])
+def guess_letter():
+    global selectedIndecies
+    base64String = request.form.to_dict()["base64Image"]
+
+    # Assuming base64_str is the string value without 'data:image/jpeg;base64,'
+    img = Image.open(io.BytesIO(base64.decodebytes(bytes(base64String, "utf-8"))))
+    img.save('testImage.jpeg')
+
+    # test image in ML get back a confidence and then adjust state...
+    # will just say its correct to test the rest of the API
+    guess = True
+
+    if(guess):
+        
+        index = selectedIndecies.index(2)
+        if(index < len(selectedIndecies) -1):
+            selectedIndecies[index] = 1
+            selectedIndecies[index+1] = 2
+        else:
+            clear_letters()
     return jsonify({
         "selectedLetters": selectedLetters,
         "selectedIndecies": selectedIndecies
